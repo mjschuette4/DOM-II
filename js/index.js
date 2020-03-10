aff19daf7bb21da3784fb27ebf4220573747fb7a
MatthewSchuette <mjschuette4@gmail.com>
2020-03-10 17:48:50 -0500

    Finished all 10

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 // Your code goes here
-// window.addEventListener("load", function(event) {
-//     alert("Never gunna give you up");
-//     alert("Never gunna let you down");
-//     alert("Never gonna run around and desert you");
-//     alert("Never gonna make you cry");
-//     alert("Never gonna say goodbye");
-//     alert("Never gonna tell a lie and hurt you");
-//     });
+
+//-----------------------------WINDOW-----------------------------------
+window.addEventListener("load", function(event) {
+    alert("Never gunna give you up");
+    alert("Never gunna let you down");
+    alert("Never gonna run around and desert you");
+    alert("Never gonna make you cry");
+    alert("Never gonna say goodbye");
+    alert("Never gonna tell a lie and hurt you");
+    });
   
+// ---------------------------IMAGES---------------------------------
+
 const img1 = document.querySelector('#image1');
 
     img1.addEventListener('mouseenter', function(event){
@@ -17,6 +21,12 @@ const img1 = document.querySelector('#image1');
     img1.addEventListener('mouseleave', function(event){
         event.target.style.border = 'none';
     });
+
+const img2 = document.querySelector('#image2');
+
+    img2.addEventListener('click', function(event){
+        event.target.style.border = ("5px solid black");
+    })
 
 const p = document.querySelector('p');
         
@@ -36,6 +46,8 @@ const navStop = document.querySelector('nav');
       alert(`YOU DIRTY DOG, YOU KNOW YOU CAN'T SEE THAT`);
     })
 
+// -------------------------BUTTONS----------------------------------
+
 const memeButton = document.querySelector('#button1');
 
     memeButton.addEventListener('dblclick', function(event){
@@ -44,9 +56,17 @@ const memeButton = document.querySelector('#button1');
     });
 
 const memeButton2 = document.querySelector('#button2');
-
     
+    memeButton2.addEventListener('contextmenu', function(event){
+        event.target.style.border = '2px solid black';
+    });
 
 const memeButton3 = document.querySelector('#button3');
 
+    memeButton3.addEventListener('mouseenter', function(event){
+        event.target.style.border = '2px dotted blue';
+    });
 
+    memeButton3.addEventListener('mouseleave', function(event){
+        event.target.style.border = 'none';
+    });
